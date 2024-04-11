5c582348ca69167b94cb43106871e66e7247b155
prince92 <prince92@moramcnt.com>
2024-04-11 13:24:13 +0900

    날짜 변경

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -6,7 +6,7 @@ from common.common_func import regist
 with DAG(
     dag_id="dags_python_with_op_args",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 4, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
